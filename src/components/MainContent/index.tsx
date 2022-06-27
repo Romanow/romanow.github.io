bbec21edb0ffe843584b7d33f6209d641fc24b5e
Romanov Alex <alexey.romanov@vrbank.ru>
2022-06-27 16:13:38 +0400

    increment

---
+++ src/components/MainContent/index.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from "react";
+
+import "./style.css";
+
+const MainContent: FC = () => {
+  return (
+      <div className="main-content">
+
+      </div>
+  )
+}
+
+export default MainContent;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import './index.css';
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
