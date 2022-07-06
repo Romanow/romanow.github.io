9c1b54ab98f27c093bfb6aa08c16cdec052953c3
aromanov <aromanov@inno.tech>
2022-07-06 15:35:04 +0400

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import About from "./components/About";
 import Header from "./components/Header";
 
 import "./App.css";
+import MainContent from "./components/MainContent";
 
 const App = () => {
     return (
         <div className="content container-fluid">
-            <Header/>
+            <div className="row">
+                <Header/>
 
-            <div className="col-4">
-              <About/>
-            </div>
+                <div className="col-4">
+                    <About/>
+                </div>  
 
-            <div className="col-8">
-
+                <div className="col-8">
+                    <MainContent/>
+                </div>
             </div>
         </div>
     );
--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -3,11 +3,27 @@ import React, {FC} from "react";
 import "./style.css";
 
 const MainContent: FC = () => {
-  return (
-      <div className="main-content">
+    return (
+        <div className="main-content">
+            <div className="container">
+                <div>
+                    <h2 className="h2">Education</h2>
+                </div>
 
-      </div>
-  )
+                <div>
+                    <h2 className="h2">Bauman Moscow State Technical University</h2>
+                </div>
+
+                <div>
+                    <h2 className="h2">Lectures and Public Reports</h2>
+                </div>
+
+                <div>
+                    <h2 className="h2">Work</h2>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MainContent;
